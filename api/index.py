a6ae34ae428b324550b233f87cbbfa28d52b125f
gggff123 <anumitkumardutta@gmail.com>
2026-04-02 21:18:49 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,6 +1,6 @@
 from flask import Flask,request,render_template
 import requests
-app=Flask(__name__)
+app = Flask(__name__, template_folder='../templates')
 @app.route('/',methods=['GET','POST'])
 def home():
     inp=request.form.get('inp')
@@ -11,5 +11,4 @@ def home():
         res=requests.get(url)
         message=res.text
     return render_template('index.html',message=message)
-if __name__ =='__main__':
-    app.run(debug=True)
+app.debug=True
